676f94f8511d9c6c8503b8510f8ebc46eb678886
bengiscelik <bengi.celik@ozu.edu.tr>
2023-05-17 22:42:23 +0300

    Google Maps Screen file added

---
+++ ReliefApp/screens/GoogleMapsScreen/GoogleMapsScreenUI.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const GoogleMapsScreenUI = () => {
+  return (
+    <View>
+      <Text>Google Maps Screen</Text>
+    </View>
+  );
+}
+
+export default GoogleMapsScreenUI;
