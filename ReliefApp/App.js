b53f574e92460aee1c7a12c2ddf9eb58c7eec789
bengicelik <82235896+bengicelik@users.noreply.github.com>
2023-05-18 18:03:27 +0300

    Update App.js - Google Map added

--- ReliefApp/App.js
+++ ReliefApp/App.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreen from './screens/WelcomeScreen/WelcomeScreen';
 import LoginScreen from './screens/LoginScreen/LoginScreen';
 import HomeScreen from './screens/HomeScreen/HomeScreen';
+import GoogleMapsScreen from './screens/GoogleMapsScreen/GoogleMapsScreen';
 
 // colors from: https://colordesigner.io/#FFD600-3C691C-ff6d1f-D8D2DD-1c1d21
 
@@ -15,12 +16,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options={{ headerShown: false }} 
-        name="Welcome" 
-        component={WelcomeScreen} 
-        />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen options={{ headerShown: false }} name="Welcome" 
+      component={WelcomeScreen} />
+     <Stack.Screen name="Login" component={LoginScreen} />
+     <Stack.Screen name="Home" component={HomeScreen} />
+     <Stack.Screen name="Map" component={GoogleMapsScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
