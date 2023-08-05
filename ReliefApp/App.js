4ac748760f0b527f776e8ec2057a690cf7e1ad2a
bengicelik <bengi.celik@ozu.edu.tr>
2023-08-06 00:32:04 +0300

    Update App.js

--- ReliefApp/App.js
+++ ReliefApp/App.js
@@ -13,21 +13,24 @@ import UserScreen from './screens/UserScreen';
 import MapScreen from './screens/MapScreen';
 import PersonalScreen from './screens/PersonalScreen';
 import DistributorScreen from './screens/DistributorScreen';
+import ChatScreen from './screens/ChatScreen';
+import MessageListScreen from './screens/MessageListScreen';
+
 
 // colors from: https://colordesigner.io/#FFD600-3C691C-ff6d1f-D8D2DD-1c1d21
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator(); 
 
 
-export default function App() {
+export default function App() { 
   return (
     <NavigationContainer>
       <Stack.Navigator>
       <Stack.Screen options={{ headerShown: false }} name="Welcome" 
       component={WelcomeScreen} />
-     <Stack.Screen name="Login" component={LoginScreen} />
-     <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
-     <Stack.Screen name="Home" component={HomeScreen} />
+     <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+     <Stack.Screen options={{ headerShown: false }} name="SignUpScreen" component={SignUpScreen} />
+     <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
      <Stack.Screen name="Map"component={GoogleMapsScreen}/> 
      <Stack.Screen name="Requested Items" component={RequestedItemsScreen} />
      <Stack.Screen name="Available Items" component={AvailableItemsScreen} />
@@ -35,6 +38,9 @@ export default function App() {
      <Stack.Screen name="Mark" component={MapScreen} />
      <Stack.Screen name="Personal" component={PersonalScreen} />
      <Stack.Screen name="Distributor" component={DistributorScreen} />
+     <Stack.Screen name="Message" component={ChatScreen} />
+     <Stack.Screen name="MessageList" component={MessageListScreen} />
+
 
       </Stack.Navigator>
     </NavigationContainer>
