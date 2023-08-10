46a1d94e99fe213800e9afa52dca6dcc40e74f37
bengicelik <bengi.celik@ozu.edu.tr>
2023-08-10 19:53:42 +0300

    Update MessageListScreen.js

--- ReliefApp/screens/MessageListScreen.js
+++ ReliefApp/screens/MessageListScreen.js
@@ -1,120 +1,9 @@
 /*import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet, Button } from 'react-native';
-import { collection, getDocs, query, where } from 'firebase/firestore';
-import { db, auth } from '../firebase';
-
-const MessageListScreen = ({ navigation }) => {
-    const [conversations, setConversations] = useState([]);
-
-    useEffect(() => {
-        const fetchConversations = async () => {
-            // Here, we're fetching all conversations for simplicity. Adjust as needed.
-            const conversationsSnapshot = await getDocs(collection(db, 'conversations'));
-            setConversations(conversationsSnapshot.docs.map(doc => doc.data()));
-        };
-
-        fetchConversations();
-    }, []);
-
-    return (
-        <View style={styles.container}>
-            <FlatList 
-                data={conversations}
-                renderItem={({ item }) => (
-                    <Button 
-                        title={`Chat with ${item.userEmail}`} 
-                        onPress={() => navigation.navigate('Messages', {userEmail: item.userEmail})} 
-                    />
-                )}
-                keyExtractor={(item, index) => index.toString()}
-            />
-        </View> 
-    );
-};
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16
-    }
-});
-
-export default MessageListScreen;
-*/
-
-
-/*
-import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
-import { collection, getDocs, query, where } from 'firebase/firestore';
-import { db, auth } from '../firebase';
-
-const MessageListScreen = () => {
-  const [conversations, setConversations] = useState([]);
-
-  useEffect(() => {
-    // Fetch messages involving the current user
-    const fetchConversations = async () => {
-      const userConversationsSnapshot = await getDocs(query(
-        collection(db, 'messages'),
-        where('from', '==', auth.currentUser.email)
-      ));
-      
-      // Combine with the messages where the current user is the recipient
-      const receivedConversationsSnapshot = await getDocs(query(
-        collection(db, 'messages'),
-        where('to', '==', auth.currentUser.email)
-      ));
-
-      const combinedConversations = [
-        ...userConversationsSnapshot.docs.map(doc => doc.data()),
-        ...receivedConversationsSnapshot.docs.map(doc => doc.data())
-      ];
-      
-      setConversations(combinedConversations);
-    };
-
-    fetchConversations();
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <FlatList 
-        data={conversations}
-        renderItem={({ item }) => (
-          <View style={styles.messageContainer}>
-            <Text>{item.from === auth.currentUser.email ? "To: " : "From: "}{item.from === auth.currentUser.email ? item.to : item.from}</Text>
-            <Text>{item.message}</Text>
-          </View>
-        )}
-        keyExtractor={(item, index) => index.toString()}
-      />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16
-    },
-    messageContainer: {
-        padding: 8,
-        borderRadius: 8,
-        backgroundColor: '#f2f2f2',
-        marginVertical: 4
-    },
-});
-
-export default MessageListScreen;
-*/
-
-import React, { useState, useEffect } from 'react';
 import { View, Text, FlatList, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { getDocs, query, where, collection } from 'firebase/firestore';
 import { db, auth } from '../firebase';
 
-const MessageListScreen = ({ navigation }) => {
+  const MessageListScreen = ({ navigation }) => {
   const [conversations, setConversations] = useState([]);
 
   useEffect(() => {
@@ -127,12 +16,14 @@ const MessageListScreen = ({ navigation }) => {
     const allMessages = messagesSnapshot.docs.map(doc => doc.data());
 
     let latestMessagesByUser = {};
+    
 
     allMessages.forEach(message => {
         let otherUser = message.from === auth.currentUser.email ? message.to : message.from;
 
         if (!latestMessagesByUser[otherUser] || message.timestamp > latestMessagesByUser[otherUser].timestamp) {
             latestMessagesByUser[otherUser] = message;
+            console.log(!latestMessagesByUser[otherUser] || message.timestamp > latestMessagesByUser[otherUser].timestamp);
         }
     });
 
@@ -158,7 +49,9 @@ return (
                 keyExtractor={(item, index) => index.toString()}
             />
         </View>
+        
     );
+
 };
 
 const styles = StyleSheet.create({
@@ -189,6 +82,96 @@ const styles = StyleSheet.create({
     messageSnippet: {
         color: 'grey',
     },
+});
+
+export default MessageListScreen;
+*/
+
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { getDocs, query, where, collection } from 'firebase/firestore';
+import { db, auth } from '../firebase';
+
+const MessageListScreen = ({ navigation }) => {
+  const [conversations, setConversations] = useState([]);
+
+  useEffect(() => {
+    fetchConversations();
+  }, []);
+
+  const fetchConversations = async () => {
+    const currentUserEmail = auth.currentUser.email;
+
+    // Fetch messages where the recipient is the current user
+    const q = query(
+      collection(db, 'messages'),
+      where('to', '==', currentUserEmail)
+    );
+
+    const messagesSnapshot = await getDocs(q);
+    const receivedMessages = messagesSnapshot.docs.map(doc => doc.data());
+
+    let latestMessagesByUser = {};
+
+    receivedMessages.forEach(message => {
+      if (!latestMessagesByUser[message.from] || message.timestamp > latestMessagesByUser[message.from].timestamp) {
+        latestMessagesByUser[message.from] = message;
+      }
+    });
+
+    setConversations(Object.values(latestMessagesByUser));
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList 
+        data={conversations}
+        renderItem={({ item }) => (
+          <TouchableOpacity 
+            onPress={() => navigation.navigate('Message', { userEmail: item.from })}
+            style={styles.listItem}
+          >
+            <Image source={{uri: 'https://cdn-icons-png.flaticon.com/512/10307/10307911.png'}} style={styles.avatar}/> 
+            <View style={styles.messageInfo}>
+              <Text style={styles.name}>{item.from}</Text>
+              <Text numberOfLines={1} ellipsizeMode='tail' style={styles.messageSnippet}>{item.message}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  listItem: {
+    flexDirection: 'row',
+    padding: 10,
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#ccc',
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 15,
+  },
+  messageInfo: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  name: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  messageSnippet: {
+    color: 'grey',
+  },
 });
 
 export default MessageListScreen;
